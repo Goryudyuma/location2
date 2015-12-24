388e425857ef05025dce34eccf9a346677e3c044
Goryudyuma <umaretekyoumade@gmail.com>
2015-12-24 16:04:46 +0900

    rfidを返すように変更

--- public/points.php
+++ public/points.php
@@ -2,7 +2,7 @@
 if (is_numeric($_REQUEST['x'])&&26.0<$_REQUEST['x']&&$_REQUEST['x']<46.0&&is_numeric($_REQUEST['y'])&&127.0<$_REQUEST['y']&&$_REQUEST['y']<146.0) {
 	$pass = explode("\n", file_get_contents('/var/www/src/PW.txt'));
 	$pdo = new PDO('mysql:dbname=location;host=localhost;charset=utf8', $pass[2], $pass[3], [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-	$sth = $pdo->prepare("SELECT section.lin AS linename, sectionid, section.opc AS opc, ST_DISTANCE(c.pos,point(:north , :east)) AS dist, sectionid FROM (SELECT * FROM curve WHERE intersects(pos, buffer(point(:north , :east), :length))) AS c INNER join (SELECT DISTINCT id, lin, opc FROM section WHERE end = 9999) AS section ON section.id = c.sectionid ORDER BY dist LIMIT 20;");
+	$sth = $pdo->prepare("SELECT section.lin AS linename, section.rfid, section.opc AS opc, ST_DISTANCE(c.pos,point(:north , :east)) AS dist, sectionid FROM (SELECT * FROM curve WHERE intersects(pos, buffer(point(:north , :east), :length))) AS c INNER join (SELECT DISTINCT id, lin, opc, rfid FROM section WHERE end = 9999) AS section ON section.id = c.sectionid ORDER BY dist LIMIT 20;");
 	$sth->bindParam(':north', $var["north"], PDO::PARAM_STR);
 	$sth->bindParam(':east', $var["east"], PDO::PARAM_STR);
 	$sth->bindParam(':length', $var["length"], PDO::PARAM_STR);
