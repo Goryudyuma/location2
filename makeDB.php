6ee1e29f1af09826b37f03a6b0182c9188fab57a
Goryudyuma <umaretekyoumade@gmail.com>
2016-01-13 00:11:57 +0900

    APIが座標点を返すようになったのと、"を'に更新

--- makeDB.php
+++ makeDB.php
@@ -19,9 +19,7 @@ try {
 		begin INTEGER NOT NULL COMMENT '開業年',
 		end INTEGER NOT NULL COMMENT '廃止年',
 		PRIMARY KEY(id),
-		INDEX(end),
-		INDEX(rfid),
-		INDEX(end, rfid)
+		INDEX(rfid)
 	) ENGINE = INNODB DEFAULT CHARSET=utf8;";
 	$pdo->exec($query);
 
@@ -38,8 +36,6 @@ try {
 		pos GEOMETRY NOT NULL COMMENT '座標',
 		PRIMARY KEY(id),
 		INDEX(sectionid),
-		INDEX(end),
-		INDEX(end, sectionid),
 		SPATIAL INDEX(pos)
 	) ENGINE = INNODB DEFAULT CHARSET=utf8;";
 	$pdo->exec($query);
@@ -73,104 +69,104 @@ try {
 	$json = file_get_contents('./N05-14_GML/N05-14.json');
 	$contents = json_decode($json, true);
 	$sth = $pdo->prepare('INSERT INTO section (`id`, `rint`, `lin`, `opc`, `rfid`, `time`, `begin`, `end`) VALUES (?, ?, ?, ?, ?, ?, ?, ?);');
-	foreach ($contents["ksj_RailroadSection2"] as $v) {
-		$sth->bindValue(1, (int) substr($v["@attributes"]["gml_id"], 3), PDO::PARAM_INT);
-		$sth->bindValue(2, (string) $v["ksj_int"], PDO::PARAM_STR);
-		$sth->bindValue(3, (string) $v["ksj_lin"], PDO::PARAM_STR);
-		$sth->bindValue(4, (string) $v["ksj_opc"], PDO::PARAM_STR);
-		$sth->bindValue(5, (int) substr($v["ksj_rfid"], 5), PDO::PARAM_INT);
-		$sth->bindValue(6, (int) $v["ksj_usb"]["gml_TimeInstant"]["gml_timePosition"], PDO::PARAM_INT);
-		$sth->bindValue(7, (int) $v["ksj_exp"]["gml_TimePeriod"]["gml_beginPosition"], PDO::PARAM_INT);
-		$sth->bindValue(8, (int) $v["ksj_exp"]["gml_TimePeriod"]["gml_endPosition"], PDO::PARAM_INT);
+	foreach ($contents['ksj_RailroadSection2'] as $v) {
+		$sth->bindValue(1, (int) substr($v['@attributes']['gml_id'], 3), PDO::PARAM_INT);
+		$sth->bindValue(2, (string) $v['ksj_int'], PDO::PARAM_STR);
+		$sth->bindValue(3, (string) $v['ksj_lin'], PDO::PARAM_STR);
+		$sth->bindValue(4, (string) $v['ksj_opc'], PDO::PARAM_STR);
+		$sth->bindValue(5, (int) substr($v['ksj_rfid'], 5), PDO::PARAM_INT);
+		$sth->bindValue(6, (int) $v['ksj_usb']['gml_TimeInstant']['gml_timePosition'], PDO::PARAM_INT);
+		$sth->bindValue(7, (int) $v['ksj_exp']['gml_TimePeriod']['gml_beginPosition'], PDO::PARAM_INT);
+		$sth->bindValue(8, (int) $v['ksj_exp']['gml_TimePeriod']['gml_endPosition'], PDO::PARAM_INT);
 		$sth->execute();
-		echo "\rsection:".sprintf("%04d", substr($v["@attributes"]["gml_id"], 3)).'/'.sizeof($contents["ksj_RailroadSection2"]);
+		echo "\rsection:".sprintf("%04d", substr($v['@attributes']['gml_id'], 3)).'/'.sizeof($contents['ksj_RailroadSection2']);
 	}
 	echo PHP_EOL;
 
 	$a=[];
-	foreach ($contents["gml_Point"] as $v) {
-		$a[$v["@attributes"]["gml_id"]]=$v["gml_pos"];
+	foreach ($contents['gml_Point'] as $v) {
+		$a[$v['@attributes']['gml_id']]=$v['gml_pos'];
 	}
 
 	$sth = $pdo->prepare('INSERT INTO station (`id`, `stn`, `sectionid`, `time`, `begin`, `end`, `pos`) VALUES (:id, :stn, :sectionid, :time, :begin, :end, POINT(:north, :east));');
 
-	$sth->bindParam(':id', $var["id"], PDO::PARAM_INT);
-	$sth->bindParam(':stn', $var["stn"], PDO::PARAM_STR);
-	$sth->bindParam(':sectionid', $var["rfid"], PDO::PARAM_INT);
-	$sth->bindParam(':time', $var["time"], PDO::PARAM_INT);
-	$sth->bindParam(':begin', $var["begin"] , PDO::PARAM_INT);
-	$sth->bindParam(':end', $var["end"], PDO::PARAM_INT);
-	$sth->bindParam(':north', $var["north"], PDO::PARAM_STR);
-	$sth->bindParam(':east', $var["east"], PDO::PARAM_STR);
+	$sth->bindParam(':id', $var['id'], PDO::PARAM_INT);
+	$sth->bindParam(':stn', $var['stn'], PDO::PARAM_STR);
+	$sth->bindParam(':sectionid', $var['rfid'], PDO::PARAM_INT);
+	$sth->bindParam(':time', $var['time'], PDO::PARAM_INT);
+	$sth->bindParam(':begin', $var['begin'] , PDO::PARAM_INT);
+	$sth->bindParam(':end', $var['end'], PDO::PARAM_INT);
+	$sth->bindParam(':north', $var['north'], PDO::PARAM_STR);
+	$sth->bindParam(':east', $var['east'], PDO::PARAM_STR);
 
-	foreach ($contents["ksj_Station2"] as $v) {
-		$var["id"] = (int) substr($v["@attributes"]["gml_id"],3);
-		$var["stn"] = (string) $v["ksj_stn"];
-		$var["rfid"] = (int) substr($v["ksj_rfid"],5,5);
-		$var["time"] = (int) $v["ksj_usb"]["gml_TimeInstant"]["gml_timePosition"];
-		$var["begin"] = (int) $v["ksj_exp"]["gml_TimePeriod"]["gml_beginPosition"];
-		$var["end"] = (int) $v["ksj_exp"]["gml_TimePeriod"]["gml_endPosition"];
-		$point = explode(' ', $a[substr($v["ksj_loc"]["@attributes"]["xlink_href"], 1)]);
-		$var["north"] = (string)(float) $point[0];
-		$var["east"] = (string)(float) $point[1];
+	foreach ($contents['ksj_Station2'] as $v) {
+		$var['id'] = (int) substr($v['@attributes']['gml_id'],3);
+		$var['stn'] = (string) $v['ksj_stn'];
+		$var['rfid'] = (int) substr($v['ksj_rfid'],5,5);
+		$var['time'] = (int) $v['ksj_usb']['gml_TimeInstant']['gml_timePosition'];
+		$var['begin'] = (int) $v['ksj_exp']['gml_TimePeriod']['gml_beginPosition'];
+		$var['end'] = (int) $v['ksj_exp']['gml_TimePeriod']['gml_endPosition'];
+		$point = explode(' ', $a[substr($v['ksj_loc']['@attributes']['xlink_href'], 1)]);
+		$var['north'] = (string)(float) $point[0];
+		$var['east'] = (string)(float) $point[1];
 
 		$sth->execute();
-		echo "\rstation:".sprintf("%04d", substr($v["@attributes"]["gml_id"], 3)).'/'.sizeof($contents["ksj_Station2"]);
+		echo "\rstation:".sprintf("%04d", substr($v['@attributes']['gml_id'], 3)).'/'.sizeof($contents['ksj_Station2']);
 	}
 	echo PHP_EOL;
 
 
 	$b=[];
-	foreach ($contents["gml_Curve"] as $v) {
-		$b[substr($v["@attributes"]["gml_id"], 3)] = explode("\n", ($v["gml_segments"]["gml_LineStringSegment"]["gml_posList"]));
-		foreach ($b[substr($v["@attributes"]["gml_id"], 3)] as &$c) {
+	foreach ($contents['gml_Curve'] as $v) {
+		$b[substr($v['@attributes']['gml_id'], 3)] = explode("\n", ($v['gml_segments']['gml_LineStringSegment']['gml_posList']));
+		foreach ($b[substr($v['@attributes']['gml_id'], 3)] as &$c) {
 			$c=explode(' ', trim($c));
 		}
 	}
 
 	$sth = $pdo->prepare('INSERT INTO curve (`id`, `sectionid`, `pos`) VALUES (NULL, :sectionid, POINT(:north, :east));');
 
-	$sth->bindParam(':sectionid', $var["sectionid"], PDO::PARAM_INT);
-	$sth->bindParam(':north', $var["north"], PDO::PARAM_STR);
-	$sth->bindParam(':east', $var["east"], PDO::PARAM_STR);
+	$sth->bindParam(':sectionid', $var['sectionid'], PDO::PARAM_INT);
+	$sth->bindParam(':north', $var['north'], PDO::PARAM_STR);
+	$sth->bindParam(':east', $var['east'], PDO::PARAM_STR);
 
 	foreach ($b as $k => $u) {
-		$var["sectionid"] = (int) substr($k, 3);
+		$var['sectionid'] = (int) substr($k, 3);
 		foreach ($u as $v) {
 			if(sizeof($v)==2){
-				$var["north"] = (string)(float) $v[0];
-				$var["east"] = (string)(float) $v[1];
+				$var['north'] = (string)(float) $v[0];
+				$var['east'] = (string)(float) $v[1];
 
 				$sth->execute();
 			}
 		}
-		echo "\rcurve:".sprintf("%04d", $var["sectionid"]).'/'.sizeof($b);
+		echo "\rcurve:".sprintf("%04d", $var['sectionid']).'/'.sizeof($b);
 	}
 	echo PHP_EOL;
 
 
 	$sth = $pdo->prepare('INSERT INTO json (`id`, `sectionid`, `json`) VALUES (NULL, :sectionid, :json);');
 
-	$sth->bindParam(':sectionid', $var["sectionid"], PDO::PARAM_INT);
-	$sth->bindParam(':json', $var["json"], PDO::PARAM_STR);
+	$sth->bindParam(':sectionid', $var['sectionid'], PDO::PARAM_INT);
+	$sth->bindParam(':json', $var['json'], PDO::PARAM_STR);
 
 	foreach ($b as $k => $u) {
-		$var["sectionid"] = (int) substr($k, 3);
+		$var['sectionid'] = (int) substr($k, 3);
 		$u = array_merge(array_filter($u, function($data){return sizeof($data) == 2;}));	
 		foreach($u as $x => $y){
 			$memo = (double)$u[$x][0];
 			$u[$x][0] = (double)$u[$x][1];
 			$u[$x][1] = $memo;
 		}
-		$var["json"] = (string) json_encode($u, JSON_UNESCAPED_UNICODE);
+		$var['json'] = (string) json_encode($u, JSON_UNESCAPED_UNICODE);
 
 		$sth->execute();
-		echo "\rjson:".sprintf("%04d", $var["sectionid"]).'/'.sizeof($b);
+		echo "\rjson:".sprintf("%04d", $var['sectionid']).'/'.sizeof($b);
 	}
 	echo PHP_EOL;
 
 	$pdo->commit();
 } catch(Exception $e) {
-	echo "Error:". $e->getMessage(). PHP_EOL;
+	echo 'Error:'. $e->getMessage(). PHP_EOL;
 	$pdo->rollback();
 }
--- public/stations.php
+++ public/stations.php
@@ -4,7 +4,7 @@ if (is_numeric($_REQUEST['x']) && 26.0 < $_REQUEST['x'] && $_REQUEST['x'] < 46.0
 	is_numeric($_REQUEST['year']) && 1950 <= $_REQUEST['year'] && $_REQUEST['year'] <= 2100) {
 	$pass = explode("\n", file_get_contents('/var/www/src/PW.txt'));
 	$pdo = new PDO('mysql:dbname=location;host=localhost;charset=utf8', $pass[2], $pass[3], [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-	$sth = $pdo->prepare('SELECT stn, section.lin AS linename, sectionid, section.opc AS opc, ST_X(pos) AS X,ST_Y(pos) AS Y, ST_DISTANCE(c.pos, POINT(:north , :east)) AS distance FROM station AS c INNER JOIN (SELECT distinct rfid, lin, opc FROM section WHERE begin <= :year AND :year <= end ) AS section ON section.rfid = c.sectionid WHERE c.begin <= :year AND :year <= c.end ORDER BY distance LIMIT 20;');
+	$sth = $pdo->prepare('SELECT stn, section.lin AS linename, sectionid, section.opc AS opc, ST_X(pos) AS north,ST_Y(pos) AS east, ST_DISTANCE(c.pos, POINT(:north , :east)) AS distance FROM station AS c INNER JOIN (SELECT distinct rfid, lin, opc FROM section WHERE begin <= :year AND :year <= end ) AS section ON section.rfid = c.sectionid WHERE c.begin <= :year AND :year <= c.end ORDER BY distance LIMIT 20;');
 	$sth->bindParam(':north', $var['north'], PDO::PARAM_STR);
 	$sth->bindParam(':east', $var['east'], PDO::PARAM_STR);
 	$sth->bindParam(':year', $var['year'], PDO::PARAM_INT);
